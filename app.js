61f100cbbf26b58fdbe75ef9186931b9afeeda3d
esseppidefi <97457479+esseppidefi@users.noreply.github.com>
2022-03-02 16:20:02 +0100

    start

---
+++ app.js
@@ -0,0 +1,12 @@
+// CREAZIONE STRUTTURA
+let campo = document.querySelector('.campo')
+for (let i = 1; i <= 100; i++) {
+    let box = document.createElement('div')
+    box.classList.add(`box-${i}`)
+    box.className = "box p-2 justify-content-center align-items-center"
+    box.style.width = "10%";
+    box.style.border = '2px, solid, black'
+    box.innerHTML = i;    
+
+    campo.append(box)
+    }

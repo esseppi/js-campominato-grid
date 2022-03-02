d13aa6d2deb4cff9f0699863f733ef144878d41f
esseppidefi <97457479+esseppidefi@users.noreply.github.com>
2022-03-02 16:25:05 +0100

    start

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ let campo = document.querySelector('.campo')
 for (let i = 1; i <= 100; i++) {
     let box = document.createElement('div')
     box.classList.add(`box-${i}`)
-    box.className = "box p-2 justify-content-center align-items-center"
+    box.className = "box d-flex p-2 justify-content-center align-items-center"
     box.style.width = "10%";
-    box.style.border = '2px, solid, black'
+    box.style.border = '2px solid black'
     box.innerHTML = i;    
 
     campo.append(box)

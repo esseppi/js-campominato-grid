c24631479b8a0f37faca5205381bf7d74d91cf87
esseppidefi <97457479+esseppidefi@users.noreply.github.com>
2022-03-02 19:15:31 +0100

    finish 1st day

--- app.js
+++ app.js
@@ -1,12 +1,46 @@
 // CREAZIONE STRUTTURA
+let side = '500px'
+let levelInp = document.querySelector('#level')
 let campo = document.querySelector('.campo')
-for (let i = 1; i <= 100; i++) {
-    let box = document.createElement('div')
-    box.classList.add(`box-${i}`)
-    box.className = "box p-2 justify-content-center align-items-center"
-    box.style.width = "10%";
-    box.style.border = '2px, solid, black'
-    box.innerHTML = i;    
-
-    campo.append(box)
+campo.style.maxWidth = '500px'
+let play = document.querySelector('.play')
+let replay = document.querySelector('.replay')
+// levels
+let easy = 49;
+let medium = 81;
+let hard = 100;
+// generazione e reset
+play.addEventListener('click', selectLev)
+replay.addEventListener('click', cancel)
+// selezione livello e generazione
+function selectLev() {
+    cancel();
+    if (levelInp.value == 'firstLevel') {
+        generazione(easy);
+    } else if (levelInp.value == 'secondLevel') {
+        generazione(medium);
+    } else  if (levelInp.value == 'thirdLevel') {
+        generazione(hard);
+    } else {
+        alert('Seleziona un livello')
+    } 
+}
+// reset
+function cancel () {
+    campo.innerHTML = ''
+    replay.classList.add('d-none')
+}
+// contenuto generato
+function generazione (level) {
+    let dim = 100 / Math.sqrt(level)
+    for (let i = 1; i <= level; i++) {
+        let box = document.createElement('div')
+        replay.classList.remove('d-none')
+        box.className = "box d-flex p-1 justify-content-center align-items-center bg-light"
+        box.classList.add(`box-${i}`)
+        box.style.border = '1px solid black'
+        box.style.width = dim + '%'
+        box.innerHTML = i;    
+        campo.append(box)
     }
+}
